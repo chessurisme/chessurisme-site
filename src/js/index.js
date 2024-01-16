02bdc5da0ce92756ffc1887da8210369132e18b3
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-01-16 08:17:12 +0800

    chore: import style for homePage

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,8 @@
   import '../css/base/color.css';
   
   // Layout
-  import '../css/layout/body.css'
+  import '../css/layout/body.css';
+  import '../css/layout/homePage.css';
 
 //* JavaScript
   // Utilities
